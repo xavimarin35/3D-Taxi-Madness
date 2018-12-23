86be3a4dc6deff83ec75181fd2b51560f130a4f3
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2018-12-23 20:51:39 +0100

    second bridge + lot more of the circuit done

--- Physics_3DGame/ModulePlayer.cpp
+++ Physics_3DGame/ModulePlayer.cpp
@@ -118,7 +118,7 @@ bool ModulePlayer::Start()
 	car.wheels[3].steering = false;
 
 	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(-265, 2, 270);
+	vehicle->SetPos(380, 2, 0);
 	
 	return true;
 }
